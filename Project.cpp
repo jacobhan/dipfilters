c87d3135dfcfd300d45b44765b0b87b1b330bcfe
jacobhan <jungwoohoo@gmail.com>
2015-08-29 02:33:45 -0700

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -4,3 +4,12 @@
 
 using namespace std;
 
+// Gaussian Filter
+int main() {
+	Image img("test.bmp");
+	img.writeG("output.bmp");
+	for (int i = 0; i < HEIGHT; i++)
+		for (int j = 0; j < WIDTH; j++)
+			cout << filteredData[i][j] << "/t";
+		cout << end1;
+}
